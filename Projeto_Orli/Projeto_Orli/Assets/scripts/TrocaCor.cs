afa65c5bbd6424d8fc6475ac77710076d086dfc5
giuhorvath <giuhorvath@gmail.com>
2025-04-01 09:26:26 -0300

    attgiu vibra

--- Projeto_Orli/Projeto_Orli/Assets/scripts/TrocaCor.cs
+++ Projeto_Orli/Projeto_Orli/Assets/scripts/TrocaCor.cs
@@ -1,37 +1,74 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TrocaCor : MonoBehaviour
 {
-
     public Color corAtivo;
     public Color corDesativo;
-   public bool ativo;
+    public bool ativo;
     public GameObject check;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private static List<TrocaCor> todosToggles = new List<TrocaCor>(); // Lista global de todos os toggles
+
     void Start()
     {
-        
+        // Adiciona este toggle à lista
+        if (!todosToggles.Contains(this))
+        {
+            todosToggles.Add(this);
+        }
+
+        // Define a cor inicial
+        AtualizarCor(ativo);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     public void TrocarCor()
     {
         if (!ativo)
         {
-            check.SetActive(true);
-            gameObject.GetComponent<Image>().color = new Color(corAtivo.r, corAtivo.g, corAtivo.b,255);
-            ativo = true;
-        }else
+            // Desativa todos os outros toggles antes de ativar este
+            foreach (var toggle in todosToggles)
+            {
+                if (toggle != this)
+                {
+                    toggle.Desativar();
+                }
+            }
+
+            Ativar();
+        }
+        else
         {
-            check.SetActive(false);
-            gameObject.GetComponent<Image>().color = new Color(corDesativo.r, corDesativo.g, corDesativo.b,255);
-            ativo = false;
+            Desativar();
         }
     }
+
+    private void Ativar()
+    {
+        check.SetActive(true);
+        gameObject.GetComponent<Image>().color = corAtivo;
+        ativo = true;
+    }
+
+    public void Desativar()
+    {
+        check.SetActive(false);
+        gameObject.GetComponent<Image>().color = corDesativo;
+        ativo = false;
+    }
+
+    private void AtualizarCor(bool estadoAtivo)
+    {
+        if (estadoAtivo)
+        {
+            Ativar();
+        }
+        else
+        {
+            Desativar();
+        }
+    }
+
+
 }
